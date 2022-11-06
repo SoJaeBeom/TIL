63dbffd9f1d41de8b0f04d908574c6e93d6d8354
SoJaeBeom <sojb0826@gmail.com>
2022-11-06 09:14:40 +0900

    [Algorithm] H-index commit

---
+++ Algorithm/알고리즘문제풀이/프로그래머스/H-index.js
@@ -0,0 +1,11 @@
+function solution(citations) {
+  citations.sort((a, b) => b - a);
+  for (let i = 0; i < citations.length; i++) {
+    if (i >= citations[i]) {
+      return i;
+    }
+  }
+  return citations.length;
+}
+
+console.log(solution([3, 0, 6, 1, 5]));
