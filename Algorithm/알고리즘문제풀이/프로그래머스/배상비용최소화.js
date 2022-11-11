0af1e047aa77676fff6903a599c1c89c990a890b
SoJaeBeom <sojb0826@gmail.com>
2022-11-12 00:08:22 +0900

    [Algorithm] 배상 비용 최소화 commit

---
+++ Algorithm/알고리즘문제풀이/프로그래머스/배상비용최소화.js
@@ -0,0 +1,74 @@
+class MaxHeap {
+  constructor() {
+    this.heap = [null];
+  }
+
+  push(value) {
+    this.heap.push(value);
+    let currentIndex = this.heap.length - 1;
+    let parentIndex = Math.floor(currentIndex / 2);
+
+    while (parentIndex !== 0 && this.heap[parentIndex] < value) {
+      const temp = this.heap[parentIndex];
+      this.heap[parentIndex] = value;
+      this.heap[currentIndex] = temp;
+
+      currentIndex = parentIndex;
+      parentIndex = Math.floor(currentIndex / 2);
+    }
+  }
+
+  pop() {
+    if (this.heap.length === 2) return this.heap.pop();
+
+    const returnValue = this.heap[1];
+    this.heap[1] = this.heap.pop();
+
+    let currentIndex = 1;
+    let leftIndex = 2;
+    let rightIndex = 3;
+
+    while (
+      this.heap[currentIndex] < this.heap[leftIndex] ||
+      this.heap[currentIndex] < this.heap[rightIndex]
+    ) {
+      if (this.heap[leftIndex] < this.heap[rightIndex]) {
+        const temp = this.heap[currentIndex];
+        this.heap[currentIndex] = this.heap[rightIndex];
+        this.heap[rightIndex] = temp;
+
+        currentIndex = rightIndex;
+      } else {
+        const temp = this.heap[currentIndex];
+        this.heap[currentIndex] = this.heap[leftIndex];
+        this.heap[leftIndex] = temp;
+
+        currentIndex = leftIndex;
+      }
+
+      leftIndex = currentIndex * 2;
+      rightIndex = currentIndex * 2 + 1;
+    }
+
+    return returnValue;
+  }
+}
+
+function solution(no, works) {
+  if (works.reduce((a, b) => a + b) <= no) {
+    return 0;
+  }
+
+  const heap = new MaxHeap();
+  for (const work of works) {
+    heap.push(work);
+  }
+
+  for (let i = 0; i < no; i++) {
+    heap.push(heap.pop() - 1);
+  }
+  return heap.heap.reduce((a, b) => a + b * b);
+}
+
+console.log(solution(4, [4, 3, 3]));
+console.log(solution(2, [3, 3, 3]));
