97d910de0f0332eac95e2254774dc81b7c154617
SoJaeBeom <sojb0826@gmail.com>
2022-11-13 16:19:41 +0900

    [Algorithm] 행렬의 곱셈 commit

---
+++ Algorithm/알고리즘문제풀이/프로그래머스/행렬의곱셈.js
@@ -0,0 +1,43 @@
+function solution(arr1, arr2) {
+  const newArr = [];
+
+  for (let i = 0; i < arr1.length; i++) {
+    let result = [];
+    for (let j = 0; j < arr2[0].length; j++) {
+      let elem = 0;
+      for (let k = 0; k < arr2.length; k++) {
+        elem += arr1[i][k] * arr2[k][j];
+      }
+      result.push(elem);
+    }
+    newArr.push(result);
+  }
+  return newArr;
+}
+console.log(
+  solution(
+    [
+      [1, 4],
+      [3, 2],
+      [4, 1],
+    ],
+    [
+      [3, 3],
+      [3, 3],
+    ]
+  )
+);
+console.log(
+  solution(
+    [
+      [2, 3, 2],
+      [4, 2, 4],
+      [3, 1, 4],
+    ],
+    [
+      [5, 4, 3],
+      [2, 4, 1],
+      [3, 1, 1],
+    ]
+  )
+);
