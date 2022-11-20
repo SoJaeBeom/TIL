22f3ce3f3106d5f8855ad56286ff4bb31e81d4ab
SoJaeBeom <sojb0826@gmail.com>
2022-11-20 09:21:09 +0900

    [Algorithm] 배달 commit

---
+++ Algorithm/알고리즘문제풀이/프로그래머스/배달.js
@@ -0,0 +1,121 @@
+// 최소힙
+class MinHeap {
+  constructor() {
+    this.heap = [null];
+  }
+
+  push(value) {
+    this.heap.push(value);
+    let currentIndex = this.heap.length - 1;
+    let parentIndex = Math.floor(currentIndex / 2);
+
+    while (parentIndex !== 0 && this.heap[parentIndex].cost > value.cost) {
+      this._swap(parentIndex, currentIndex);
+
+      currentIndex = parentIndex;
+      parentIndex = Math.floor(currentIndex / 2);
+    }
+  }
+
+  pop() {
+    if (this.isEmpty()) return;
+    if (this.heap.length === 2) return this.heap.pop();
+
+    const returnValue = this.heap[1];
+    this.heap[1] = this.heap.pop();
+
+    let currentIndex = 1;
+    let leftIndex = 2;
+    let rightIndex = 3;
+    while (
+      (this.heap[leftIndex] &&
+        this.heap[currentIndex].cost > this.heap[leftIndex].cost) ||
+      (this.heap[rightIndex] &&
+        this.heap[currentIndex].cost > this.heap[rightIndex].cost)
+    ) {
+      if (this.heap[leftIndex] === undefined) {
+        this._swap(rightIndex, currentIndex);
+      } else if (this.heap[rightIndex] === undefined) {
+        this._swap(leftIndex, currentIndex);
+      } else if (this.heap[leftIndex].cost > this.heap[rightIndex].cost) {
+        this._swap(rightIndex, currentIndex);
+      } else if (this.heap[leftIndex].cost <= this.heap[rightIndex].cost) {
+        this._swap(leftIndex, currentIndex);
+      }
+      leftIndex = currentIndex * 2;
+      rightIndex = currentIndex * 2 + 1;
+    }
+
+    return returnValue;
+  }
+
+  isEmpty() {
+    return this.heap.length === 1;
+  }
+
+  _swap(a, b) {
+    [this.heap[a], this.heap[b]] = [this.heap[b], this.heap[a]];
+  }
+}
+
+// 최단 경로 알고리즘 -> 다익스트라 알고리즘
+function dijkstra(road, N) {
+  const heap = new MinHeap();
+  heap.push({ node: 1, cost: 0 });
+
+  const dist = [...Array(N + 1)].map(() => Infinity);
+  dist[1] = 0;
+
+  while (!heap.isEmpty()) {
+    const { node: current, cost: currentCost } = heap.pop();
+
+    for (const [src, dest, cost] of road) {
+      const nextCost = cost + currentCost;
+
+      if (src === current && nextCost < dist[dest]) {
+        dist[dest] = nextCost;
+        heap.push({ node: dest, cost: nextCost });
+      } else if (dest == current && nextCost < dist[src]) {
+        dist[src] = nextCost;
+        heap.push({ node: src, cost: nextCost });
+      }
+    }
+  }
+
+  return dist;
+}
+
+function solution(N, road, K) {
+  const dist = dijkstra(road, N);
+  return dist.filter((x) => x <= K).length;
+}
+
+console.log(
+  solution(
+    5,
+    [
+      [1, 2, 1],
+      [2, 3, 3],
+      [5, 2, 2],
+      [1, 4, 2],
+      [5, 3, 1],
+      [5, 4, 2],
+    ],
+    3
+  )
+);
+console.log(
+  solution(
+    6,
+    [
+      [1, 2, 1],
+      [1, 3, 2],
+      [2, 3, 2],
+      [3, 4, 3],
+      [3, 5, 2],
+      [3, 5, 3],
+      [5, 6, 1],
+    ],
+    4
+  )
+);
